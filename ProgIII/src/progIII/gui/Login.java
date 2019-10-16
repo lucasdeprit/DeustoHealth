4ed7ee2958d0fcf68f202a65c447871e581b6bfa
aitor <aitor@aitor-OMEN-by-HP-Laptop>
2019-10-16 12:17:16 +0200

    Trabajo de Joel

--- ProgIII/src/progIII/gui/Login.java
+++ ProgIII/src/progIII/gui/Login.java
@@ -7,6 +7,7 @@ import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -42,7 +43,7 @@ public class Login {
 
 	private void initialize() { // definir contenidos ventana para inicializarlos
 		frame = new JFrame();
-		frame.setBounds(100,100,450,300); // definir tamaño ventana
+		frame.setBounds(100,100,450,300); // definir tamaï¿½o ventana
 		frame.getContentPane().setBackground(SystemColor.textHighlight);	//color del fondo de la ventana
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null); // definir distribucion componentes en la ventana
@@ -50,7 +51,7 @@ public class Login {
 		
 		
 		JButton btnLogin = new JButton("INICIAR SESION"); // crear boton
-		btnLogin.addActionListener(new ActionListener() { // definir accion que ejecutará el boton
+		btnLogin.addActionListener(new ActionListener() { // definir accion que ejecutarï¿½ el boton
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
@@ -59,9 +60,9 @@ public class Login {
 		});
 		btnLogin.setForeground(new Color(51,0,51)); // primer plano del boton
 		btnLogin.setBackground(Color.LIGHT_GRAY); // color de fondo del boton
-		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 9)); // tipo de letra y tamaño
+		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 9)); // tipo de letra y tamaï¿½o
 		btnLogin.setBounds(72,191,123,52); // definir los bordes del boton (localizacion)
-		frame.getContentPane().add(btnLogin); // añadir el boton a nuestra ventana
+		frame.getContentPane().add(btnLogin); // aï¿½adir el boton a nuestra ventana
 		
 
 		JButton btnRegister = new JButton("REGISTRARSE");
@@ -94,19 +95,19 @@ public class Login {
 		frame.getContentPane().add(User_textField);
 		User_textField.setColumns(10);
 		
-		User_passwordField = new JPasswordField();		//se utiliza para cuando queremos guardar una cotraseña, ya que siempre es algo que no se debe ver, debido a la seguridad del usuario
+		User_passwordField = new JPasswordField();		//se utiliza para cuando queremos guardar una cotraseï¿½a, ya que siempre es algo que no se debe ver, debido a la seguridad del usuario
 		User_passwordField.setBounds(130, 135, 214, 30);
 		frame.getContentPane().add(User_passwordField);
 		
 		JLabel user_lbl = new JLabel("");
-		user_lbl.setHorizontalAlignment(SwingConstants.CENTER);							 //definimos el punto de referencia para la alineación horizontal que seleccionamos
-		//user_lbl.setIcon(new ImageIcon(Login.class.getResource("/icons8-user-30.png"))); //ruta de la imagen que situaremos en este label.
+		user_lbl.setHorizontalAlignment(SwingConstants.CENTER);							 //definimos el punto de referencia para la alineaciï¿½n horizontal que seleccionamos
+		user_lbl.setIcon(new ImageIcon(Login.class.getResource("/icons8-user-30.png"))); //ruta de la imagen que situaremos en este label.
 		user_lbl.setBounds(89, 82, 32, 30);												 //User icon by Icons8
 		frame.getContentPane().add(user_lbl);
 		
 		JLabel password_lbl = new JLabel("");
 		password_lbl.setHorizontalAlignment(SwingConstants.CENTER);
-		//password_lbl.setIcon(new ImageIcon(Login.class.getResource("/icons8-password-1-48.png"))); //Password1 icon by Icons8
+		password_lbl.setIcon(new ImageIcon(Login.class.getResource("/icons8-password-1-48.png"))); //Password1 icon by Icons8
 		password_lbl.setBounds(84, 123, 38, 45);
 		frame.getContentPane().add(password_lbl);
 		
