3c90d3b55a384085a6c90a79d3a44fd97a04a963
aitormorais <aitormorais@opendeusto.es>
2019-11-11 19:47:05 +0100

    Implementando Calendar... no finalizado

--- ProgIII/src/progIII/gui/Calendar.java
+++ ProgIII/src/progIII/gui/Calendar.java
@@ -2,12 +2,23 @@ package progIII.gui;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import com.toedter.calendar.JCalendar;
+import com.toedter.calendar.JMonthChooser;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.awt.event.ActionEvent;
 
 public class Calendar {
 
 	private JFrame frame;
-
+	//private JMonthChooser mes;
+	private String mes;
+	private SimpleDateFormat sdf;
 	/**
 	 * Launch the application.
 	 */
@@ -35,9 +46,26 @@ public class Calendar {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		sdf =  new SimpleDateFormat("MM");
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton btnAceptar = new JButton();
+		
+		btnAceptar.setText("Aceptar");
+		JCalendar calendar = new JCalendar();
+		calendar.setTodayButtonVisible(true);
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(btnAceptar.isSelected()) {
+					calendar.getMonthChooser().getMonth();
+									}
+				
+			}
+		});
+		calendar.setTodayButtonText("Seleccionon Dia Actual");
+		frame.getContentPane().add(calendar, BorderLayout.CENTER);
+		frame.getContentPane().add(btnAceptar,BorderLayout.SOUTH);
 	}
 
 }
