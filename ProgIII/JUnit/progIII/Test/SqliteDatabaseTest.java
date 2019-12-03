242c55017f8bd0bceb37fe45500966e5bf29929f
lucasdeprit <luak@luakLaptop>
2019-12-03 09:03:51 +0100

    sqlitedatabaseTest

---
+++ ProgIII/JUnit/progIII/Test/SqliteDatabaseTest.java
@@ -0,0 +1,14 @@
+package progIII.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SqliteDatabaseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
